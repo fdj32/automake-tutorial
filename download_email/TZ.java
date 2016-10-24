09d88f03739dacd3ef67ae267ba40876e3e55f89
Nick Feng <Nick.Feng@activenetwork.com>
2016-10-24 15:45:00 +0800

    add timezone test

--- download_email/TZ.java
+++ download_email/TZ.java
@@ -1,3 +1,5 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.TimeZone;
 
 
@@ -7,6 +9,11 @@ public class TZ {
 		for(String str : TimeZone.getAvailableIDs()) {
 			System.out.println(str);
 		}
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Calendar c6 = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT-6"));
+		System.out.println("Etc/GMT-6:" + sdf.format(c6.getTime()));
+		Calendar c8 = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT-8"));
+		System.out.println("Etc/GMT-8:" + sdf.format(c8.getTime()));
 	}
 
 }
