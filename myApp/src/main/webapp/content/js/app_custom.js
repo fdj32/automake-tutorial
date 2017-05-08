603cfe4cf84957b6ba20c3af4a75d784ed10038f
nickfeng <nickfeng@10.110.120.162>
2017-05-08 17:18:05 +0800

    menu toggle

--- myApp/src/main/webapp/content/js/app_custom.js
+++ myApp/src/main/webapp/content/js/app_custom.js
@@ -1,4 +1,4 @@
-var CURRENT_URL = window.location.href.split('#')[0].split('?')[0], $BODY = $('body'), $MENU_TOGGLE = $('#menu_toggle'), $SIDEBAR_MENU = $('#sidebar-menu'), $SIDEBAR_FOOTER = $('.sidebar-footer'), $LEFT_COL = $('.left_col'), $RIGHT_COL = $('.right_col'), $NAV_MENU = $('.nav_menu'), $FOOTER = $('footer');
+var $BODY = $('body'), $MENU_TOGGLE = $('#menu_toggle');
 
 // toggle small or large menu
 $MENU_TOGGLE.on('click', function() {
