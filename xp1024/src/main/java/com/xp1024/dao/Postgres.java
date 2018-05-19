0e376f23098de4ec3a7d68b3428c19fea0874f9c
Nick Feng <fdj32@163.com>
2018-05-19 15:01:27 +0800

    use yml replace properties, use default jdbcTemplate

--- xp1024/src/main/java/com/xp1024/dao/Postgres.java
+++ xp1024/src/main/java/com/xp1024/dao/Postgres.java
@@ -2,49 +2,29 @@ package com.xp1024.dao;
 
 import java.util.Map;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Postgres {
 
-	@Bean(name = "datasource2")
-	@ConfigurationProperties(prefix = "spring.datasource2")
-	public DataSource secondaryDataSource() {
-		return DataSourceBuilder.create().build();
-	}
-
-	@Bean(name = "jdbcTemplate2")
 	@Autowired
-	JdbcTemplate jdbcTemplate2(@Qualifier("datasource2") DataSource ds) {
-		JdbcTemplate jt = new JdbcTemplate(ds);
-		return jt;
-	}
-
-	@Autowired
-	@Qualifier("jdbcTemplate2")
-	private JdbcTemplate jdbcTemplate2;
+	private JdbcTemplate jdbcTemplate;
 
 	public long count() {
-		Map<String, Object> map = jdbcTemplate2.queryForMap("select count(1) as total from htmdata");
+		Map<String, Object> map = jdbcTemplate.queryForMap("select count(1) as total from htmdata");
 		return (long) map.get("total");
 	}
 	
 	public void save(int fid, String link, String title, String data) {
 		String sql = "insert into htmdata(fid, link, title, data, data_length) values (?, ?, ?, ?, ?)";
-		jdbcTemplate2.update(sql, new Object[] { fid, link, title, data, data.length() });
+		jdbcTemplate.update(sql, new Object[] { fid, link, title, data, data.length() });
 	}
 
 	public long queryByLinkAndTitle(String link, String title) {
 		String sql = "select count(1) as total from htmdata where link=? and title=?";
-		Map<String, Object> map = jdbcTemplate2.queryForMap(sql, link, title);
+		Map<String, Object> map = jdbcTemplate.queryForMap(sql, link, title);
 		return (long) map.get("total");
 	}
 
