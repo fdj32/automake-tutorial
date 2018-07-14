400137d5dfe5ea98339cbb1a07f12d43fdf5ea92
Nick Feng <fdj32@163.com>
2018-07-14 12:47:00 +0800

    ...

--- xp1024/src/main/java/com/xp1024/job/JsoupJob.java
+++ xp1024/src/main/java/com/xp1024/job/JsoupJob.java
@@ -21,26 +21,12 @@ public class JsoupJob {
 
 	private static final int RETRY_TIMES = 2;
 
-	private static final String BASE = "http://s3.97xzl.xyz/";
+	private static final String BASE = "http://w3.nat789.info/";
 
 	private static final Logger LOG = LoggerFactory.getLogger(JsoupJob.class);
 
 	@Autowired
 	private Postgres pg;
-
-	private Document get(int fid, int page) throws IOException {
-		return connect(BASE + String.format("forum.php?mod=forumdisplay&fid=%d&page=%d", fid, page));
-	}
-
-	private int last(int fid) throws IOException {
-		Document doc = get(fid, 1);
-		Element e = doc.selectFirst("a .last");
-		if(null == e)
-			return 0;
-		int last = Integer.parseInt(e.text().split(" ")[1]);
-		LOG.info("fid={}, pages={}", fid, last);
-		return last;
-	}
 
 	@Scheduled(fixedRate = 600000)
 	public void jsoup() throws IOException {
@@ -51,6 +37,85 @@ public class JsoupJob {
 		long endCount = pg.count();
 		LOG.info("startCount={}, endCount={}, {} added", startCount, endCount, (endCount - startCount));
 		LOG.info("jsoup() finished in {} seconds", (System.currentTimeMillis() - start) / 1000);
+	}
+
+	private void getfids() {
+		Document doc = connect(BASE);
+		Elements elements = doc.select("#category_1 tr");
+		elements.parallelStream().forEach(this::tr);
+	}
+
+	private void tr(Element e) {
+		if (null == e)
+			return;
+		Element td = e.selectFirst(".bk_a3");
+		if (null == td)
+			return;
+		Element php = td.selectFirst(".l");
+		if (null == php)
+			return;
+		int pFid = Integer.parseInt(php.attr("href").split("=")[1]);
+		String pTitle = php.text();
+		pg.saveThread(pFid, pTitle, -1);
+		Elements elements = td.select("a");
+		elements.parallelStream().forEach(i -> forumPhp(i, pFid));
+	}
+
+	private void forumPhp(Element e, int pFid) {
+		if (null == e)
+			return;
+		if (e.hasClass("l"))
+			return;
+		String href = e.attr("href");
+		String title = e.text();
+		try {
+			int fid = Integer.parseInt(href.split("-")[1]);
+			pg.saveThread(fid, title, pFid);
+			fid2db(fid);
+		} catch (NumberFormatException e1) {
+			LOG.error("forumPhp({}, {})", href, pFid);
+		}
+	}
+
+	private void fid2db(int fid) {
+		int last;
+		try {
+			last = last(fid);
+			if (0 == last)
+				return;
+			IntStream.range(1, last + 1).parallel().forEach(i -> fidPage2db(fid, i));
+		} catch (IOException e) {
+			LOG.error("Failed in fid2db({})", fid);
+		}
+	}
+
+	private int last(int fid) throws IOException {
+		Document doc = get(fid, 1);
+		Element e = doc.selectFirst("a .last");
+		if (null == e)
+			return 0;
+		int last = Integer.parseInt(e.text().split(" ")[1]);
+		LOG.info("fid={}, pages={}", fid, last);
+		return last;
+	}
+
+	private void fidPage2db(int fid, int page) {
+		Document doc = null;
+		try {
+			doc = get(fid, page);
+			if (null == doc)
+				return;
+			Elements elements = doc.select("#threadlisttableid tbody tr td .max-td table tbody tr td .new");
+			elements.parallelStream().forEach(i -> fidPageLinks(fid, page, i));
+		} catch (Exception e) {
+			LOG.error("Failed in fidPage({}, {})", fid, page);
+			return;
+		}
+	}
+
+	private void fidPageLinks(int fid, int page, Element i) {
+		String line = i.attr("href") + "," + i.text();
+		processLine(line, fid);
 	}
 
 	private void processLine(String s, int fid) {
@@ -84,6 +149,10 @@ public class JsoupJob {
 		}
 	}
 
+	private Document get(int fid, int page) throws IOException {
+		return connect(BASE + String.format("forum.php?mod=forumdisplay&fid=%d&page=%d", fid, page));
+	}
+
 	private Document connect(String url) {
 		Document doc = null;
 		int times = 1;
@@ -102,72 +171,4 @@ public class JsoupJob {
 		return doc;
 	}
 
-	private void getfids() {
-		Document doc = connect(BASE);
-		Elements elements = doc.select("#category_1 tr");
-		elements.parallelStream().forEach(this::tr);
-	}
-
-	private void tr(Element e) {
-		if (null == e)
-			return;
-		Element td = e.selectFirst(".bk_a3");
-		if (null == td)
-			return;
-		Element php = td.selectFirst(".l");
-		if(null == php)
-			return;
-		int pFid = Integer.parseInt(php.attr("href").split("=")[1]);
-		String pTitle = php.text();
-		pg.saveThread(pFid, pTitle, -1);
-		Elements elements = td.select("a");
-		elements.parallelStream().forEach(i -> forumPhp(i, pFid));
-	}
-	
-	private void forumPhp(Element e, int pFid) {
-		if(null == e)
-			return;
-		if(e.hasClass("l"))
-			return;
-		String href = e.attr("href");
-		String title = e.text();
-		try {
-			int fid = Integer.parseInt(href.split("-")[1]);
-			pg.saveThread(fid, title, pFid);
-			fid2db(fid);
-		} catch (NumberFormatException e1) {
-			LOG.error("forumPhp({}, {})", href, pFid);
-		}
-	}
-
-	private void fid2db(int fid) {
-		int last;
-		try {
-			last = last(fid);
-			if(0 == last)
-				return;
-			IntStream.range(1, last + 1).parallel().forEach(i -> fidPage2db(fid, i));
-		} catch (IOException e) {
-			LOG.error("Failed in fid2db({})", fid);
-		}
-	}
-
-	private void fidPage2db(int fid, int page) {
-		Document doc = null;
-		try {
-			doc = get(fid, page);
-			if (null == doc)
-				return;
-			Elements elements = doc.select("#threadlisttableid tbody tr td .max-td table tbody tr td .new");
-			elements.parallelStream().forEach(i -> fidPageLinks(fid, page, i));
-		} catch (Exception e) {
-			LOG.error("Failed in fidPage({}, {})", fid, page);
-			return;
-		}
-	}
-
-	private void fidPageLinks(int fid, int page, Element i) {
-		String line = i.attr("href") + "," + i.text();
-		processLine(line, fid);
-	}
 }
