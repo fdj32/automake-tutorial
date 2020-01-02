0cbcf1e5997faf878ea4a15398fcd102682f89fc
Nick Feng <Nick.Feng@activenetwork.com>
2020-01-02 15:07:19 +0800

    merge primary result set into column result set.

--- dbimport/src/main/java/io/github/fdj32/util/DatabaseMetaDataUtil.java
+++ dbimport/src/main/java/io/github/fdj32/util/DatabaseMetaDataUtil.java
@@ -16,22 +16,6 @@ public class DatabaseMetaDataUtil {
         ResultSet rs = metaData.getTables(catalog, schema, null, null);
         while (rs.next()) {
             String table = rs.getString(3);
-            System.out.println(table);
-            ResultSet rsPk = metaData.getPrimaryKeys(catalog, schema, table);
-            List<String[]> pkList = new ArrayList<>();
-            pkList.add(new String[]{"TABLE_CAT", "TABLE_SCHEM", "TABLE_NAME", "COLUMN_NAME", "KEY_SEQ", "PK_NAME"});
-            StringBuilder sbPK = new StringBuilder();
-            while (rsPk.next()) {
-                pkList.add(new String[]{
-                        defaultNull(rsPk.getString(1)),
-                        defaultNull(rsPk.getString(2)),
-                        defaultNull(rsPk.getString(3)),
-                        defaultNull(rsPk.getString(4)),
-                        defaultNull(rsPk.getString(5)),
-                        defaultNull(rsPk.getString(6))
-                });
-            }
-            BeautifyOutputUtil.printTable(pkList);
 
             ResultSet rsColumn = metaData.getColumns(catalog, schema, table, null);
             List<String[]> columnList = new ArrayList<>();
@@ -39,9 +23,8 @@ public class DatabaseMetaDataUtil {
                     "TYPE_NAME", "COLUMN_SIZE", "BUFFER_LENGTH", "DECIMAL_DIGITS", "NUM_PREC_RADIX",
                     "NULLABLE", "REMARKS", "COLUMN_DEF", "SQL_DATA_TYPE", "SQL_DATETIME_SUB",
                     "CHAR_OCTET_LENGTH", "ORDINAL_POSITION", "IS_NULLABLE", "SCOPE_CATALOG", "SCOPE_SCHEMA",
-                    "SCOPE_TABLE", "SOURCE_DATA_TYPE", "IS_AUTOINCREMENT", "IS_GENERATEDCOLUMN"});
-
-            StringBuilder sbColumn = new StringBuilder();
+                    "SCOPE_TABLE", "SOURCE_DATA_TYPE", "IS_AUTOINCREMENT", "IS_GENERATEDCOLUMN",
+                    "PRIMARY_KEY", "KEY_SEQ", "PK_NAME"});
 
             while (rsColumn.next()) {
                 columnList.add(new String[]{
@@ -72,10 +55,22 @@ public class DatabaseMetaDataUtil {
                         defaultNull(rsColumn.getString(21)),
                         rsColumn.getShort(22) + "",
                         defaultNull(rsColumn.getString(23)),
-                        defaultNull(rsColumn.getString(24))
+                        defaultNull(rsColumn.getString(24)),
+                        "", "", ""
                 });
-                BeautifyOutputUtil.printTable(columnList);
+
+                ResultSet rsPk = metaData.getPrimaryKeys(catalog, schema, table);
+                while (rsPk.next()) {
+                    for (String[] array : columnList) {
+                        if (array[3].equals(rsPk.getString(4))) {
+                            array[24] = "YES";
+                            array[25] = rsPk.getString(5);
+                            array[26] = rsPk.getString(6);
+                        }
+                    }
+                }
             }
+            BeautifyOutputUtil.printTable(columnList);
         }
     }
 }
