16fe110e0723254bebadf118bd188532d394414e
Nick Feng <Nick.Feng@activenetwork.com>
2020-01-02 16:25:11 +0800

    toMySQL is almost ok

--- dbimport/src/main/java/io/github/fdj32/util/DatabaseMetaDataUtil.java
+++ dbimport/src/main/java/io/github/fdj32/util/DatabaseMetaDataUtil.java
@@ -1,5 +1,7 @@
 package io.github.fdj32.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -7,6 +9,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseMetaDataUtil {
+
+    private static final String MSSQLSERVER = "MSSQLSERVER";
+    private static final String MySQL = "MySQL";
+
+    private static final String[][] DATA_TYPE_MAP = new String[][]{
+            new String[]{MSSQLSERVER, MySQL},
+            new String[]{"int", "INTEGER"},
+            new String[]{"char", "CHAR"},
+            new String[]{"varchar", "VARCHAR"},
+            new String[]{"datetime", "DATETIME"},
+            new String[]{"int identity", "INTEGER AUTOINCREMENT"},
+            new String[]{"money", "DECIMAL"},
+            new String[]{"tinyint", "TINYINT"},
+            new String[]{"uniqueidentifier", "VARCHAR"},
+            new String[]{"varbinary", "VARBINARY"},
+            new String[]{"bit", "BIT"}
+    };
 
     private static String defaultNull(String s) {
         return null == s ? "null" : s;
@@ -71,6 +90,50 @@ public class DatabaseMetaDataUtil {
                 }
             }
             BeautifyOutputUtil.printTable(columnList);
+            toMysql(columnList);
         }
+    }
+
+    private static void toMysql(List<String[]> list) {
+        if (null == list || list.size() < 2)
+            return;
+        String table = list.get(1)[2];
+        StringBuilder sb = new StringBuilder();
+        sb.append("DROP TABLE ").append(table).append(";\n");
+        sb.append("CREATE TABLE ").append(table).append(" (\n");
+        for (int i = 1; i < list.size(); i++) {
+            sb.append("\t").append(list.get(i)[3]).append(" ");
+            sb.append(dataType(list.get(i)[5], MSSQLSERVER, MySQL));
+            if ("0".equals(list.get(i)[10])) {
+                sb.append(" NOT NULL");
+            }
+            if(i != list.size() - 1)
+                sb.append(",");
+            sb.append("\n");
+        }
+        sb.append(");");
+        System.out.println(sb.toString());
+    }
+
+    private static String dataType(String srcDataType, String srcDatabase, String destDatabase) {
+        if (StringUtils.isEmpty(srcDataType) || StringUtils.isEmpty(srcDatabase) || StringUtils.isEmpty(destDatabase)) {
+            return null;
+        }
+        int srcIndex = 0;
+        int destIndex = 0;
+        for (int i = 0; i < DATA_TYPE_MAP[0].length; i++) {
+            if (DATA_TYPE_MAP[0][i].equals(srcDatabase)) {
+                srcIndex = i;
+            }
+            if (DATA_TYPE_MAP[0][i].equals(destDatabase)) {
+                destIndex = i;
+            }
+        }
+        for (int i = 1; i < DATA_TYPE_MAP.length; i++) {
+            if (srcDataType.equals(DATA_TYPE_MAP[i][srcIndex])) {
+                return DATA_TYPE_MAP[i][destIndex];
+            }
+        }
+        return null;
     }
 }
