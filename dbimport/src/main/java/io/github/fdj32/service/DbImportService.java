4ec4950dc2373d1d50e6ace1c1bc18138a1bd733
Nick Feng <Nick.Feng@activenetwork.com>
2020-01-02 13:36:33 +0800

    showTables is ok

--- dbimport/src/main/java/io/github/fdj32/service/DbImportService.java
+++ dbimport/src/main/java/io/github/fdj32/service/DbImportService.java
@@ -30,23 +30,62 @@ public class DbImportService {
 
     public void importToMysql() throws SQLException {
         LOG.info("begin");
-        DatabaseMetaData md = odsJdbcTemplate.getDataSource().getConnection().getMetaData();
+        showTables(odsJdbcTemplate);
+        showTables(mysqlJdbcTemplate);
+        LOG.info("end");
+    }
+
+    private void showTables(JdbcTemplate jdbcTemplate) throws SQLException {
+        DatabaseMetaData md = jdbcTemplate.getDataSource().getConnection().getMetaData();
         ResultSet rs = md.getTables(CATALOG, SCHEMA, null, null);
         while (rs.next()) {
             String table = rs.getString(3);
             System.out.println(table);
             ResultSet rsPk = md.getPrimaryKeys(CATALOG, SCHEMA, table);
+            StringBuilder sbPK = new StringBuilder();
             while (rsPk.next()) {
-                System.out.println("PK: " + rsPk.getString(4));
+                sbPK.append("TABLE_CAT:\t").append(rsPk.getString(1)).append("\t");
+                sbPK.append("TABLE_SCHEM:\t").append(rsPk.getString(2)).append("\t");
+                sbPK.append("TABLE_NAME:\t").append(rsPk.getString(3)).append("\t");
+                sbPK.append("COLUMN_NAME:\t").append(rsPk.getString(4)).append("\t");
+                sbPK.append("KEY_SEQ:\t").append(rsPk.getString(5)).append("\t");
+                sbPK.append("PK_NAME:\t").append(rsPk.getString(6)).append("\n");
             }
+            System.out.println(sbPK.toString());
             ResultSet rsColumn = md.getColumns(CATALOG, SCHEMA, table, null);
+            StringBuilder sbColumn = new StringBuilder();
             while (rsColumn.next()) {
-                System.out.println("Column: " + rsColumn.getString(4));
+                sbColumn.append("TABLE_CAT:\t").append(rsColumn.getString(1)).append("\t");
+                sbColumn.append("TABLE_SCHEM:\t").append(rsColumn.getString(2)).append("\t");
+                sbColumn.append("TABLE_NAME:\t").append(rsColumn.getString(3)).append("\t");
+                sbColumn.append("COLUMN_NAME:\t").append(rsColumn.getString(4)).append("\t");
+                sbColumn.append("DATA_TYPE:\t").append(rsColumn.getInt(5)).append("\t");
+
+                sbColumn.append("TYPE_NAME:\t").append(rsColumn.getString(6)).append("\t");
+                sbColumn.append("COLUMN_SIZE:\t").append(rsColumn.getInt(7)).append("\t");
+                sbColumn.append("BUFFER_LENGTH:\t").append(rsColumn.getString(8)).append("\t");
+                sbColumn.append("DECIMAL_DIGITS:\t").append(rsColumn.getInt(9)).append("\t");
+                sbColumn.append("NUM_PREC_RADIX:\t").append(rsColumn.getInt(10)).append("\t");
+
+                sbColumn.append("NULLABLE:\t").append(rsColumn.getInt(11)).append("\t");
+                sbColumn.append("REMARKS:\t").append(rsColumn.getString(12)).append("\t");
+                sbColumn.append("COLUMN_DEF:\t").append(rsColumn.getString(13)).append("\t");
+                sbColumn.append("SQL_DATA_TYPE:\t").append(rsColumn.getInt(14)).append("\t");
+                sbColumn.append("SQL_DATETIME_SUB:\t").append(rsColumn.getInt(15)).append("\t");
+
+                sbColumn.append("CHAR_OCTET_LENGTH:\t").append(rsColumn.getInt(16)).append("\t");
+                sbColumn.append("ORDINAL_POSITION:\t").append(rsColumn.getInt(17)).append("\t");
+                sbColumn.append("IS_NULLABLE:\t").append(rsColumn.getString(18)).append("\t");
+                sbColumn.append("SCOPE_CATALOG:\t").append(rsColumn.getString(19)).append("\t");
+                sbColumn.append("SCOPE_SCHEMA:\t").append(rsColumn.getString(20)).append("\t");
+
+                sbColumn.append("SCOPE_TABLE:\t").append(rsColumn.getString(21)).append("\t");
+                sbColumn.append("SOURCE_DATA_TYPE:\t").append(rsColumn.getShort(22)).append("\t");
+                sbColumn.append("IS_AUTOINCREMENT:\t").append(rsColumn.getString(23)).append("\t");
+                sbColumn.append("IS_GENERATEDCOLUMN:\t").append(rsColumn.getString(24)).append("\n");
             }
+            System.out.println(sbColumn.toString());
             System.out.println(StringUtils.repeat("-", 80));
         }
-        LOG.info("end");
-
-
     }
 }
