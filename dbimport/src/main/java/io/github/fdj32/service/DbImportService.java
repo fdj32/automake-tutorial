50f5fcfc56b9ca8c875ed5345ae83fb3307708f9
Nick Feng <Nick.Feng@activenetwork.com>
2020-01-07 10:53:32 +0800

    DataReplicateUtil is ok

--- dbimport/src/main/java/io/github/fdj32/service/DbImportService.java
+++ dbimport/src/main/java/io/github/fdj32/service/DbImportService.java
@@ -1,5 +1,6 @@
 package io.github.fdj32.service;
 
+import io.github.fdj32.util.DataReplicateUtil;
 import io.github.fdj32.util.DatabaseMetaDataUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,13 +30,23 @@ public class DbImportService {
 
     public void importToMysql() throws SQLException {
         LOG.info("begin");
-        showTables(odsJdbcTemplate);
-        showTables(mysqlJdbcTemplate);
+//        showTables(odsJdbcTemplate);
+//        showTables(mysqlJdbcTemplate);
+        replicate();
         LOG.info("end");
     }
 
     private void showTables(JdbcTemplate jdbcTemplate) throws SQLException {
         DatabaseMetaData metaData = jdbcTemplate.getDataSource().getConnection().getMetaData();
         DatabaseMetaDataUtil.showTables(metaData, CATALOG, SCHEMA);
+    }
+
+    private void replicate() {
+        String select = "SELECT account_id, account_ms_id, account_type, account_zip, account_owner, account_date_created, account_date_updated, account_address, account_state, account_country, instance_id, account_sub_type, account_number_pre_six FROM account_info WHERE account_id >= ? AND account_id < ?";
+        String insert = "INSERT INTO account_info (account_id, account_ms_id, account_type, account_zip, account_owner, account_date_created, account_date_updated, account_address, account_state, account_country, instance_id, account_sub_type, account_number_pre_six) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        int min = 1;
+        int max= 10000;
+        int batchSize = 1000;
+        DataReplicateUtil.replicate(odsJdbcTemplate, select, mysqlJdbcTemplate, insert, min, max, batchSize);
     }
 }
---
+++ dbimport/src/main/java/io/github/fdj32/util/DataReplicateUtil.java
@@ -0,0 +1,33 @@
+package io.github.fdj32.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class DataReplicateUtil {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DataReplicateUtil.class);
+
+    /**
+     * @param from      Source Database JdbcTemplate
+     * @param select    select col1, col2, col3 from src_table where id >= index and id < index+batchSize
+     * @param to        Dest Database JdbcTemplate
+     * @param insert    insert into table dest_table values (col1Value, col2Value, col3Value);
+     * @param min       min value of src_table id
+     * @param max       max value of src_table id
+     * @param batchSize batchSize of select from src_table and insert into dest_table
+     */
+    public static void replicate(JdbcTemplate from, String select, JdbcTemplate to, String insert, int min, int max, int batchSize) {
+        for (int index = min; index < max; index += batchSize) {
+            List<Map<String, Object>> resultList = from.queryForList(select, index, index + batchSize);
+            if (null == resultList || 0 == resultList.size())
+                return;
+            int[] returnCodes = to.batchUpdate(insert, resultList.stream().map(m -> m.values().toArray()).collect(Collectors.toList()));
+            LOG.info("{} rows affected", returnCodes.length);
+        }
+    }
+}
